bb63dfbdda69d290962b36228220548e0bdab06e
Jacob Garcia <jacobgarcia@jacobs-mbp.lan>
2022-11-07 23:16:18 -0600

    controls

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
+    [SerializeField] float turnSpeed = 0.5f;
+    [SerializeField] float driveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +16,10 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        float steerAmount = -Input.GetAxis("Horizontal") * turnSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * driveSpeed;
+        transform.Rotate(0, 0, steerAmount);
+        transform.Translate(0, moveAmount, 0);
 
     }
 }
