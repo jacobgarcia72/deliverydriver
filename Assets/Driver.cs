922548ff6241210df65cb863265c673e09e53bf0
Jacob Garcia <jacobgarcia@aggienetwork.com>
2022-11-09 18:42:37 -0600

    cleanup

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -8,12 +8,6 @@ public class Driver : MonoBehaviour
     int turnSpeed = 10;
     int driveSpeed = 15;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float moveAmount = Input.GetAxis("Vertical") * driveSpeed * Time.deltaTime;
